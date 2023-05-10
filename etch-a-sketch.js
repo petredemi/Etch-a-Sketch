31b14da70a7863b5c313fe6f6cdace41e9455227
petrudemian <petredemi@gmail.com>
2023-05-10 20:42:15 +0100

    create button to clean canvas

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,33 +3,48 @@
 
 
 let canvas = 5;
+let divs = '';
 
+function rest(){
+    arr.forEach((divs) => {
+    divs.classList.remove('col');
+    divs.textContent = '';
+});
+}
 
 const container = document.querySelector('.container');
 const btn = document.querySelector('#btn');
 container.setAttribute('style', `diplay: inline-grid; grid-template-columns: repeat(${canvas}, 1fr)`);
 
+
+
 function  hovering(){
 
     for( let i = 1; i <= canvas * canvas; i++){
-        const divs = document.createElement('div');
+        divs = document.createElement('div');
         divs.classList.add('color');
         container.appendChild (divs);
 
-        divs.addEventListener('mouseover', (e) => {
-            divs.classList.add('col'); 
-            divs.textContent = "ds";  
-        });
-    }   
-}
-container.addEventListener('click', (e) => {
-    e.target.classList.remove('col');
-    e.target.textContent = '';
-    console.log(e);
 
-});
+       divs.addEventListener('mouseover', (e) => {
+            e.target.classList.add('col'); 
+            e.target.textContent = "ds";  
+
+        });
+        divs.addEventListener('click', (e) => {
+    
+            e.target.textContent = '';
+            e.target.classList.remove('col');
+        });
+    }      
+}
+
 hovering();
 
+const arr = Array.from(document.querySelectorAll('.container > .color'));
+btn.addEventListener('click', () => {
+    rest();
+});
 
 
 
