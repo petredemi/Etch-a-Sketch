b4dd722cb67af708534aa9f4916980f3f3674909
petrudemian <petredemi@gmail.com>
2023-05-27 10:50:49 +0100

    added some events on canvas

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,61 +1,115 @@
 
 
 let divs = '';
-
+let canvas = 50;
 
 const btn = document.querySelector('#btn');
-const color = document.querySelector('#color');
 const pixels = document.querySelector('#pixels');
+const inputs = document.querySelectorAll('#grid, #pan, #back');
+const control = document.querySelector('.mycanvas > .control');
 const container = document.querySelector('.container');
 
-const inputColor = document.querySelectorAll('.control #pen');
-const inputCanvas = document.querySelectorAll('.control #pixels');
-let canvas = document.getElementById('pixels').value;
+
+pa = document.createElement('div');
+pa.classList.add('numb');
+control.appendChild(pa);
+const numb = document.querySelector('.container > .numb');
+let b;
 
 
+//pixels.addEventListener('change', pixelUpdate);
+
+//pixels.addEventListener('change', () => {
+  //  document.getElementById('demo').innerHTML = pixels.value;
+    //canvas = document.getElementById('demo').innerHTML;
+
+    //return canvas;
+//});
+function a(){
+    var  x = document.getElementById('pixels').value;
+    document.getElementById('demo').innerHTML = x;
+    
+}
+
+const changeV = () => {
+    pa.textContent = pixels.value;
+    canvas = this.pixels.value;
+ //   document.documentElement.setProperty(`--${this.name}`, this.value);
+
+    return canvas;
+
+  //  console.log(canvas);
+}
 function rest(){
     arr.forEach((divs) => {
     divs.classList.remove('col');
+   // container.remove('div');
+    pixels.value = 20;
+    pa.textContent = 20;
 });
+} 
+function ad(){
+    divs.classList.add('col');
 }
 
-/*function canvasSize() {
-    const sz = this.dataset.value || '';
-    document.documentElement.setProperty(`--${this.name}`, this.value + sz);
-    console.log(sz);
-
-}*/
-function penColor() {
+function handelUpdate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-  }
-//inputCanvas.forEach(input => input.addEventListener('change', canvas));
 
-inputColor.forEach(input => input.addEventListener('change', penColor));
-inputColor.forEach(input => input.addEventListener('mouseover', penColor));
+}
+inputs.forEach(input => input.addEventListener('change', handelUpdate));
+inputs.forEach(input => input.addEventListener('click', handelUpdate));
 
-container.setAttribute('style', `diplay: inline-grid; grid-template-columns: repeat(${canvas}, 1fr)`);    
 
-function  hovering(){
-    
+function hovering(canvas){
+    container.setAttribute('style', `diplay: grid; grid-template-columns:repeat(${canvas}, 1fr)`);    
+
     for( let i = 1; i <= canvas * canvas; i++){
         divs = document.createElement('div');
         divs.classList.add('color');
         container.appendChild (divs);
-
-        divs.addEventListener('mousemove', (e) => {
-               e.target.classList.add('col')
-            });
-
-        divs.addEventListener('click', (e) => {
-            e.target.classList.remove('col');
-        });
     }
-
 }
-hovering();
+changeV();
+pixels.addEventListener('change', changeV);
+//pixels.addEventListener('click', changeV);
+
+
+hovering(canvas);
 
 const arr = Array.from(document.querySelectorAll('.color'));
+
+let isDown = false;
+
+arr.forEach((div) => div.addEventListener('mousedown', (e) => {
+    isDown = true;
+    e.target.classList.add('col');
+}));
+
+arr.forEach((div) => div.addEventListener('mouseup', (e) => {
+    isDown = false;
+ }));
+
+arr.forEach((div) => div.addEventListener('mouseenter', (e) => {
+
+}));
+arr.forEach((div) => div.addEventListener('click', (e) => {
+    isDown = false;
+    e.target.classList.remove('col');
+ //   console.log(isDown);
+
+ }));
+ 
+arr.forEach((div) => div.addEventListener('mouseover', (e) => {
+    if(!isDown) return; //stop function to run
+    e.target.classList.add('col');
+    console.log(isDown);
+    console.log('do work');
+}));
 btn.addEventListener('click', () => {
     rest();
 });
+
+
+
+
